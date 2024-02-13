92fc35dd08372f6c6db9623e1b460d8f70bd4381
vathasalya <vathsalya1999@gmail.com>
2024-02-13 17:59:51 -0500

    first set of changes

---
+++ ichdemo/app.py
@@ -0,0 +1,130 @@
+import os
+import pydicom
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, request, render_template, url_for, redirect
+from magicsauce import * 
+import keras
+import tensorflow as tf
+import cv2
+
+config = tf.ConfigProto(
+    device_count={'GPU': 1},
+    intra_op_parallelism_threads=1,
+    allow_soft_placement=True
+)
+
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.6
+
+session = tf.Session(config=config)
+
+keras.backend.set_session(session)
+
+model = keras.models.load_model('2019-12-04-01_23_28.857681-08_00-ResNet50-conv+head-670k-epoch=04-valid-loss=0.06.hdf5', custom_objects={'correct_positive_diagnoses': correct_positive_diagnoses})
+model._make_predict_function()
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route("/")
+def fileFrontPage():
+    return render_template('base.html')
+
+@app.route("/handleUpload", methods=['POST'])
+def handleFileUpload():
+    if 'brainscan' in request.files:
+        dcm = request.files['brainscan']
+        if dcm.filename != ' ':
+            ds = pydicom.dcmread(dcm)
+
+        dcmarray = ds.pixel_array
+        fname = dcm.filename
+
+        img = window_and_scale_brain_subdural_soft(ds)
+        img = cv2.resize(img, INPUT_SHAPE[:2], interpolation=cv2.INTER_LINEAR)
+        tensor = np.expand_dims(img, axis=0)
+
+        graph = tf.compat.v1.get_default_graph()
+        with session.as_default():
+        	with session.graph.as_default():
+        		p = model.predict(tensor)
+
+        print(p)
+        fname = dcm.filename
+        max_intensity = np.max(dcmarray)
+        plt.imshow(dcmarray, cmap=plt.cm.bone)
+        plt.savefig('static/images/'+fname+'_raw_img.png')
+        metadata = metaprint(ds)
+        bsb= bsb_window(ds, fname)
+    return render_template('index.html', max=max_intensity, metadata= metadata, 
+        url = '/static/images/'+fname+'_raw_img.png', windowing = bsb, prediction=p)
+
+def metaprint(dataset):
+    metadata = []
+    for data_element in dataset:
+        if data_element.name == 'Pixel Data':
+            name = data_element.name
+            value = 'Array of 524288 elements'
+        else:
+            name = data_element.name
+            value = data_element.value
+        metadata.append({'name' : name, 'value': value})
+    return metadata
+
+def correct_dcm(dcm):
+    x = dcm.pixel_array + 1000
+    px_mode = 4096
+    x[x>=px_mode] = x[x>=px_mode] - px_mode
+    dcm.PixelData = x.tobytes()
+    dcm.RescaleIntercept = -1000
+
+def window_image(dcm, window_center, window_width):
+    
+    if (dcm.BitsStored == 12) and (dcm.PixelRepresentation == 0) and (int(dcm.RescaleIntercept) > -100):
+        correct_dcm(dcm)
+    
+    img = dcm.pixel_array * dcm.RescaleSlope + dcm.RescaleIntercept
+    img_min = window_center - window_width // 2
+    img_max = window_center + window_width // 2
+    img = np.clip(img, img_min, img_max)
+
+    return img
+
+def bsb_window(dcm, filename):
+    brain_img = window_image(dcm, 40, 80)
+    subdural_img = window_image(dcm, 80, 200)
+    bone_img = window_image(dcm, 600, 2800)
+    
+    brain_img = (brain_img - 0) / 80
+    subdural_img = (subdural_img - (-20)) / 200
+    bone_img = (bone_img + 800) / 2800
+
+    plt.imshow(brain_img, cmap=plt.cm.bone)
+    plt.title('Brain Matter')
+    bmpath = 'static/images/'+filename+'_brain_img.png'
+    plt.savefig(bmpath)
+
+    plt.imshow(subdural_img, cmap=plt.cm.bone)
+    plt.title('Subdural/Blood')
+    sdpath = 'static/images/'+filename+'_subdural_img.png'
+    plt.savefig(sdpath)
+
+    #plt.imshow(bone_img, cmap=plt.cm.bone)
+    plt.imshow(bone_img, cmap=plt.cm.bone)
+    plt.title('Bone')
+    bopath = 'static/images/'+filename+'_bone.png'
+    plt.savefig(bopath)
+
+    windowing = [ 
+    {'url': bmpath}, 
+    {'url': sdpath},
+    {'url': bopath}]
+
+    #bsb_img = np.array([brain_img, subdural_img, soft_img]).transpose(1,2,0)
+
+    return windowing
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9999)    
+
+
